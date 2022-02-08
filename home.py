ab51ce3a5883521a7b1352bc4710adcadc400617
alexstef90 <geo.stefanov@gmail.com>
2022-02-08 15:08:39 +0300

    home.py added

---
+++ home.py
@@ -0,0 +1,15 @@
+a = input('первое число')
+b = input('второе число')
+c = input('третье число')
+a = float(a)
+b = int(b)
+c = int(c)
+d = a**2+b**2
+e=3*b-4
+f = (d/e)
+g=4*c**5
+i = g/6
+h=f//i
+print(h)
+s=f%i
+print(s)
