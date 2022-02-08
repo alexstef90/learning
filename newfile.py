9b7a1e673d5dfde29e8bfd8e19328baade21bde3
alexstef90 <geo.stefanov@gmail.com>
2022-02-08 15:45:19 +0300

    newfile.py added

--- newfile.py
+++ newfile.py
@@ -1,2 +1,19 @@
-print (14+8)
-print (18+12)
+s1=input('первая строка')
+s2=input('вторая строка')
+print (len(s1))
+print (len(s2))
+print (s1[0])
+print (s2[-1])
+print (s1 == s2)
+print (s1 in s2)
+print (s2 in s1)
+
+
+
+
+
+
+
+
+
+
